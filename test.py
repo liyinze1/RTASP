ecc589f0bfc00d64d171d7f799501451d42e561f
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-06-23 11:02:26 +0200

    1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # init command
-cmd = "arecord -Dac108 -f S32_LE -r 48000 -c 4"
+cmd = "arecord -Dac108 -f S32_LE -r 48000 -c 4 -d 10"
 
 # excute ffmpeg command
 pipe = subprocess.run(cmd,
