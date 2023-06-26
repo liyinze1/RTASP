a86464589f0be855c0f7e5d928855eb18d0fb92a
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-06-26 16:14:38 +0200

    1

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import subprocess
-
+import shlex
 # init command
 cmd = "arecord -Dac108 -f S32_LE -r 48000 -c 4 -d 2"
+cmd = shlex.split(cmd)
 
 # excute ffmpeg command
-pipe = subprocess.run(cmd,
+pipe = subprocess.Popen(cmd,
                     stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    shell=True
+                    stderr=subprocess.STDOUT,
+                    bufsize=1000
                     )
 
 
