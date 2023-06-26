31c82b671ad21e36286cf2ea92ef582f08688254
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-06-26 16:27:25 +0200

    1

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import subprocess
 import shlex
 # init command
+
+
 cmd = "arecord -Dac108 -f S32_LE -r 48000 -c 4 -d 2"
 cmd = shlex.split(cmd)
 
@@ -8,10 +10,10 @@ cmd = shlex.split(cmd)
 pipe = subprocess.Popen(cmd,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT,
-                    bufsize=1000
+                    # bufsize=1000
                     )
 
 
 # print(pipe.stderr)
 print(type(pipe.stdout))
-# print(len(pipe.stdout))
+print(len(pipe.stdout))
