adf0e5381b5f176a6ad3e6ec02672569f74a9ba7
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-06-26 19:01:56 +0200

    1

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import shlex
 # init command
 
 
-cmd = "arecord -Dac108 -f S32_LE -r 48000 -c 4 -d 1"
+cmd = 'arecord -Dac108 -f S32_LE -r 48000 -c 4 -d 1'
 cmd = shlex.split(cmd)
 
 # excute ffmpeg command
@@ -14,7 +14,13 @@ pipe = subprocess.Popen(cmd,
                     )
 
 
-# print(pipe.stderr)
-s = pipe.stdout.read()
-print(type(s))
-print(len(s))
+# header
+s = pipe.stdout.read(124)
+
+n = 0
+
+while pipe.poll() is not None:
+    frame = pipe.stdout.read(16)
+    n += 1
+    
+print(n)
