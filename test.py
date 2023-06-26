590206e4f1c07973724b06a2abd72cf52fc60174
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-06-26 20:03:38 +0200

    1

--- test.py
+++ test.py
@@ -6,29 +6,33 @@ import shlex
 cmd = 'arecord -Dac108 -f S32_LE -r 48000 -c 4 -d 10'
 cmd = shlex.split(cmd)
 
-# excute ffmpeg command
-pipe = subprocess.Popen(cmd,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT,
-                    # bufsize=1000
-                    )
+size_list = [4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 7680, 15360]
+
+for size in size_list:
+
+    # excute ffmpeg command
+    pipe = subprocess.Popen(cmd,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        # bufsize=1000
+                        )
 
 
-# header
-s = pipe.stdout.read(124)
+    # header
+    s = pipe.stdout.read(124)
 
-size = 7680
-count = 0
+    count = 0
 
-s = bytes(0)
+    s = bytes(0)
 
-while True:
-    frame = pipe.stdout.read(size)
-    if len(frame) == 0:
-        break
-    else:
-        count += 1
+    while True:
+        frame = pipe.stdout.read(size)
+        if len(frame) == 0:
+            break
+        else:
+            count += 1
 
-print(frame)
-print(count * size)
-print((count * size) / 7680000)
+    print('------------------')
+    print(size)
+    print(count * size)
+    print((count * size) / 7680000)
