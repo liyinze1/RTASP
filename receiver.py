d5f36d60d715be197643e423010d017f2aa3d634
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-06-29 11:37:42 +0200

    1

--- receiver.py
+++ receiver.py
@@ -26,7 +26,7 @@ try:
         #     break
         # head, addr = sock.recvfrom(16)
         # t += head
-        data, addr = sock.recvfrom(128)
+        data, addr = sock.recvfrom(256)
         # print(type(data))
         # break
         t += len(data)
