89a9b1798fc9b4448656ff532b70b1eb917d90a6
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-01-16 16:14:47 +0100

    test

---
+++ random_data_sender.py
@@ -0,0 +1,27 @@
+import time
+import random
+from RTASP import *
+
+class random_data(sensor):
+    
+    def __init__(self, id, packet_size=4096):
+        self.id = id
+        self.packet_size = packet_size
+        self.active = False
+        
+        
+    def get_data(self):
+        time.sleep(1)
+        return random.randbytes(self.packet_size)
+
+sender = RTASP_sender(dest_ip='10.147.19.97', sender_ip='10.147.19.232', repeat_duration=1, repeat=3)
+data = random_data(0)
+sender.register(data)
+
+
+
+# sender.start(0)
+
+# time.sleep(10)
+
+# sender.stop(0)
