b8c772193e8ca172721deea4b6933ef805661b24
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-01-23 19:52:53 +0100

    test

--- audio_sender.py
+++ audio_sender.py
@@ -10,23 +10,10 @@ class audio_sensor(sensor):
         self.packet_size = packet_size
         
     def start(self):
-        cmd = 'ffmpeg -re -i ../test.wav -acodec copy -'
-        cmd = shlex.split(cmd)
-        self.pipe = subprocess.Popen(cmd,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT,
-                            bufsize=self.packet_size * 10
-                            )
-        self.active = True
+        self.f = open('../test.wav', 'rb')
         
     def get_data(self):
-        while not self.active:
-            pass
-        return self.pipe.stdout.read(self.packet_size)
-    
-    def stop(self):
-        self.active = False
-        self.pipe.kill()
+        return self.f.read(self.packet_size)
         
 audio = audio_sensor(0)
 
