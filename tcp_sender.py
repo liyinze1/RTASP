cfce2b7cb86e38052b8e77991dbe86f7f51a0b24
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-05-03 01:02:55 +0200

    update

--- tcp_sender.py
+++ tcp_sender.py
@@ -12,10 +12,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     # Send data repeatedly
     try:
+        start_time = time.time()
+        total_bytes = 0
         while True:
             data = random.randbytes(4096)
             s.sendall(data)
             # time.sleep(0.001)
+            total_bytes += len(data)
+
+            # Calculate elapsed time
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+
+            if elapsed_time >= 1:  # Each second
+                print(total_bytes / elapsed_time / 1024)
+                # Reset timer and byte count
+                start_time = current_time
+                total_bytes = 0
+                # time.sleep(0.001)
     except KeyboardInterrupt:
         print("Stopped by user.")
 
--- udp_sender.py
+++ udp_sender.py
@@ -6,12 +6,30 @@ def send_random_data(host='3.123.215.67', port=9924):
     # Create a socket object for UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         try:
+            
+            start_time = time.time()
+            total_bytes = 0
+            
             while True:
                 # Generate random data
                 
-                s.sendto(random.randbytes(4096), (host, port))
+                data = random.randbytes(4096)
+                
+                s.sendto(data, (host, port))
 
                 # Wait for 2 seconds before sending next data
+                
+                total_bytes += len(data)
+
+                # Calculate elapsed time
+                current_time = time.time()
+                elapsed_time = current_time - start_time
+
+                if elapsed_time >= 1:  # Each second
+                    print(total_bytes / elapsed_time / 1024)
+                    # Reset timer and byte count
+                    start_time = current_time
+                    total_bytes = 0
                 # time.sleep(0.001)
         except KeyboardInterrupt:
             print("Stopped by user.")
