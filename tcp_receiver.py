4a9589ca1a9a5db4188d85bf318f1b1b741a4b08
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-05-03 00:33:53 +0200

    test

---
+++ tcp_receiver.py
@@ -0,0 +1,42 @@
+import socket
+import time
+import threading
+
+class bandwidth_calculator(object):
+    def __init__(self,):
+        self.count = 0
+        self.print_thread = threading.Thread(target=self.print)
+        self.print_thread.start()
+        
+    def update(self, c):
+        self.count += c
+        
+    def print(self):
+        time.sleep(1)
+        print(self.count, end='\r')
+        self.count = 0
+        
+
+host='0.0.0.0'
+port=9924
+calculator = bandwidth_calculator()
+# Create a socket object
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Bind the socket to the address and port
+    s.bind((host, port))
+    # Enable the server to accept connections
+    s.listen()
+    print("Server is listening on", host, port)
+
+    # Wait for a connection
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        
+        while True:
+            data = conn.recv(4096)
+            calculator.update(len(data))
+            
+            
+
+                
---
+++ tcp_sender.py
@@ -0,0 +1,21 @@
+import socket
+import random
+import time
+
+
+host='3.123.215.67'
+port=9924
+# Create a socket object
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Connect to the server
+    s.connect((host, port))
+
+    # Send data repeatedly
+    try:
+        while True:
+            data = random.randbytes(4096)
+            s.sendall(data.encode())
+            time.sleep(0.001)
+    except KeyboardInterrupt:
+        print("Stopped by user.")
+
