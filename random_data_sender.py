f0450472d91924fcead9c9e4de08856f6229a97f
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-05-03 01:45:53 +0200

    1024

--- random_data_sender.py
+++ random_data_sender.py
@@ -4,7 +4,7 @@ from RTASP import *
 
 class random_data(sensor):
     
-    def __init__(self, id, packet_size=2048):
+    def __init__(self, id, packet_size=1024):
         self.id = id
         self.packet_size = packet_size
         self.active = False
